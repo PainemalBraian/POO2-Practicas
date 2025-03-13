0739a89a4076e408a1eb3ad2612fdd024f118436
Braian <braian089@hotmail.com>
2025-03-13 20:31:45 -0300

    Avance anemia y correciones

---
+++ src/main/java/ar/com/unrn/tp0/anemica/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        // Instancia el tiempo actual
+        Tiempo tiempo = new Tiempo();
+        // Formato largo
+        String formatoLargo = tiempo.getFecha().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
+        System.out.println("Fecha en formato largo: " + formatoLargo);
+        // Formato corto
+        String formatoCorto = tiempo.getFecha().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        System.out.println("Fecha en formato corto: " + formatoCorto);
+    }
+}
